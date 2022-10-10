580a65dc89050bfbab8d18fcfe22364033ce4502
AursuleseiStefan <40248249+AursuleseiStefan@users.noreply.github.com>
2022-10-10 12:49:43 +0300

    Create p4.py

---
+++ Lab1/p4.py
@@ -0,0 +1,16 @@
+def transformString(s):
+    s = s.replace(s[0], s[0].lower(), 1)
+    for i in range(1, len(s)):
+        if 'A' <= s[i] <= 'Z':
+            s = s.replace(s[i], s[i].lower(), 1)
+            s = s[:i] + '_' + s[i:]
+    if 'A' <= s[-1] <= 'Z':
+        s = s.replace(s[-1], s[-1].lower(), 1)
+        s = s[:-1] + '_' + s[-1]
+
+    return s
+
+
+if __name__ == '__main__':
+    string = input("Enter string: ")
+    print(transformString(string))
