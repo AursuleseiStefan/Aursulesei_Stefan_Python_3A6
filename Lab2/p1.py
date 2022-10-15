0a539dd98a0e32a3ad95d40e10b4fb2751c0e27d
AursuleseiStefan <40248249+AursuleseiStefan@users.noreply.github.com>
2022-10-15 16:12:36 +0300

    Update p1.py

--- Lab2/p1.py
+++ Lab2/p1.py
@@ -1,10 +1,18 @@
+fibona=[]
 def Fibo(n):
-    if n==0:
-        return 0
-    elif n==1 or n==2:
+    if(n==2 or n==1):
         return 1
+    elif(n==0):
+        return 0
     else:
-        return Fibo(n-2)+Fibo(n-1)
+        for i in range(0,n):
+            if(i==0):
+                fibona.append(0)
+            elif(i==2 or i==1):
+                fibona.append(1)
+            elif(i>2):
+                fibona.append(fibona[i-1]+fibona[i-2])
+    return fibona
 
 if __name__ == '__main__':
     print (Fibo(9))
