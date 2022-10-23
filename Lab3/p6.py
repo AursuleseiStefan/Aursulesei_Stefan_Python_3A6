8c71d9aba477f67338f26bd769f0218f257c0430
AursuleseiStefan <40248249+AursuleseiStefan@users.noreply.github.com>
2022-10-23 14:02:43 +0300

    Update p6.py

--- Lab3/p6.py
+++ Lab3/p6.py
@@ -1,7 +1,7 @@
-def tupla(lista):
-     a=set(lista)
-     dubla=len(lista)-len(a)
-     tupl=(a,dubla)
+def tupla(lista) :
+     a = set(lista)
+     dubla = len(lista) - len(a)
+     tupl = (a , dubla)
      return tupl
 
-print(tupla([1,2,3,4,3,4]))
+print(tupla([1 , 2 , 3 , 4, 3 , 4]))
